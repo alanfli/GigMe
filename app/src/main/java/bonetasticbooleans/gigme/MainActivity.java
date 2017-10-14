4a3d988a06ce6287c9f4decc6623cb43587f5b53
david-w-key <dwarrenkey27@gmail.com>
2017-10-13 23:35:15 -0400

    Adding App Shell

---
+++ app/src/main/java/bonetasticbooleans/gigme/MainActivity.java
@@ -0,0 +1,14 @@
+package bonetasticbooleans.gigme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import bonetasticbooleans.gigme.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
